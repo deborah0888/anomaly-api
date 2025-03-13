4fa2eff7cae7a3989fb98566ff46107742c823ac
akshitha4-hub <komuravellyakshitha.com>
2025-03-13 16:25:04 +0530

    home page styling done

---
+++ client/src/components/hero.jsx
@@ -0,0 +1,19 @@
+// components/Hero.js
+import React from 'react';
+import './Hero.css';
+import VideoShowcase from './VideoShowcase';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1 className="hero-title">Smart Anomaly Detection with AI-Powered Precision</h1>
+      <p className="hero-subtitle">Enhancing Industrial Quality with Synthetic Data & AI-driven Insights.</p>
+      <button className="cta-button">Upload file</button>
+      <p className="no-credit">Visualizing Anomalies, Optimizing Performance</p>
+      
+      <VideoShowcase />
+    </section>
+  );
+};
+
+export default Hero;
